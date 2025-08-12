af31db51b6c91cf7e318d0cac9aeca91f3766f85
Sev7ein <0914473@my.scccd.edu>
2025-08-12 12:01:39 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//HelloWorld.java
+// SeveinNieves 8/12/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World and Welcome");
+    }
+}
